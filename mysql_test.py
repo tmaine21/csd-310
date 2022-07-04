e3f6d3404fc83b4e66b167397a0f39764efd94a0
tmaine21 <106488948+tmaine21@users.noreply.github.com>
2022-07-03 21:14:45 -0500

    Add files via upload

---
+++ mysql_test.py
@@ -0,0 +1,40 @@
+""" 
+    Title: mysql_test.py
+    Author: Tanner Maine
+    Date: 3 July 2022
+    Description: Connection to pytestdatabase
+"""
+
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    "user": "pysports_user",
+    "password": "MySQL8IsGreat!",
+    "host": "127.0.0.1",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
+
+try:
+
+    db = mysql.connector.connect(**config) 
+     
+    print("\n  Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+
+    input("\n\n  Press any key to continue...")
+
+except mysql.connector.Error as err:
+
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("  The supplied username or password are invalid")
+
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("  The specified database does not exist")
+
+    else:
+        print(err)
+
+finally:
+
+    db.close()
